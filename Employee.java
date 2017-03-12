0a277bf7ab98b00d6c47ed99f2412890ebc96784
kkrowsky <krowsky@uwm.edu>
2017-03-12 09:40:08 -0500

    Int to Long
    phone number and department were swapped. Also, must be a long. Int is
    not large enough

--- Employee.java
+++ Employee.java
@@ -2,10 +2,10 @@ public class Employee {
 	
 	private String _lastName;
 	private String _firstName;
-	private int _phoneNumber;
+	private long _phoneNumber;
 	private String _department;
 	
-	public Employee(String firstName, String lastName, int phoneNumber, String department ) {
+	public Employee(String firstName, String lastName, String department, long phoneNumber) {
 		_lastName = lastName;
 		_firstName = firstName;
 		_phoneNumber = phoneNumber;
